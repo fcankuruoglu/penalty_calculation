ccab2f198109642ffeb3c4457a1fcd753be91311
Ferruhcan Kuruoğlu <ferruhcankuruoglu@gmail.com>
2022-09-29 11:17:33 +0300

    Initial commit.

---
+++ src/main/java/com/penaltycalculation/PenaltyCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.penaltycalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenaltyCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenaltyCalculationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/penaltycalculation/PenaltyCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penaltycalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenaltyCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
